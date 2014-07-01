0a980c73eea896444d0ab0694cc321369c03d7e4
Damian Jason Lapidge <greydamian@users.noreply.github.com>
2014-07-01 23:56:19 +0100

    Added copyright info to build script

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env bash
 
+# Copyright (c) 2014 Damian Jason Lapidge
+#
+# The contents of this file are subject to the terms and conditions defined 
+# within the file LICENSE.txt, located within this project's root directory.
+
 CC="cc"; # c compiler command (can be hard-coded to "gcc" or "clang")
 
 SOURCE="netbat.c";
